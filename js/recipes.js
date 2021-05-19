31510614b07dede137bab7e910b3d3b50655319d
simon <simon.florysiak@gmail.com>
2021-05-19 22:35:08 +0200

    dropdown data

--- js/recipes.js
+++ js/recipes.js
@@ -1,5 +1,6 @@
 export class recipes {
     constructor(data){
+        //this.recettes = data;
         this.id = data.id;
         this.name = data.name;
         this.servings = data.servings;
@@ -9,24 +10,28 @@ export class recipes {
         this.appliance = data.appliance;
         this.ustensils = data.ustensils
 
-        this.recipesSection = document.querySelector(".recette"); // photographes  index.html
+        this.appareilsList = document.getElementById("appareilsList");
+        this.ingredientsList = document.getElementById("ingredientsList"); 
+        this.ustensilesList = document.getElementById("ustensilesList");
+        this.recipesSection = document.querySelector(".recette");
 
     }
 
     dropdown(){
+        
         const searchList = document.querySelectorAll(".searchList");
         const searchListText = document.querySelectorAll(".searchListText");
         for (let i = 0; i < searchList.length; i++) {
             searchList[i].addEventListener('click', () => {
-				console.log(searchList[i].childNodes)
-                console.log(searchListText[i].childNodes)
-
+				//console.log(searchList[i].childNodes)
+                //console.log(searchListText[i].childNodes)
                 searchList[i].classList.toggle('open');
                 if  (searchList[i].classList.contains('open')){
                     searchListText[i].childNodes[1].style.display = "block";
+                    searchListText[i].childNodes[1].focus();
                     searchListText[i].childNodes[3].style.display = "none";
                     searchListText[i].childNodes[5].style.transform = "rotate(180deg)";
-                    searchList[i].childNodes[3].style.display = "block";
+                    searchList[i].childNodes[3].style.display = "grid";
                 }
                 else {
                     searchListText[i].childNodes[1].style.display = "none";
@@ -36,10 +41,20 @@ export class recipes {
                 }
 			});
           }
-
-       
     }
 
+    search(){
+        
+        const searchBar = document.getElementById("searchbar")
+        searchBar.addEventListener("keypress", myFunction);
+
+        function myFunction() {
+            if (searchBar.value.length > 1){
+            console.log('test');
+            }
+        }
+
+    }
     toHTML(){
         this.recipesSection.innerHTML += ` <article class="recetteArticle">
                     <div class="recetteImg"></div>
@@ -57,6 +72,36 @@ export class recipes {
 		            </div>
                 </article>
         `
+    }
+    toHTMLList(){
+        /*const ustensiles = document.getElementById("ustensilesList");
+        const appareils = document.getElementById("appareilsList");
+        const ingredients = document.getElementById("ingredientsList");
+        console.log(ustensiles,appareils,ingredients)*/
 
+        this.appareilsList.innerHTML += `<li class="appareilsListItem">
+                                        <button class="button">${this.appliance}</button>
+                                    </li>`
+
+        this.ingredientsList.innerHTML += `${this.ingredients.map(ingredient => `<li class="ingredientsListItem"><button class="button">${ingredient.ingredient}</button></li>`).join('')}`
+		
+       this.ustensilesList.innerHTML += `${this.ustensils.map(ustensil => `<li class="ustensilesListItem"><button class="button">${ustensil}</button></li>`).join('')}`
+
+    }
+
+    filterDuplicate(media,value) {
+        this.media = media;
+        this.cleanGallery();
+        switch (value)
+            {
+                case "Popularity":
+                this.media.sort((a, b) => b.likes - a.likes); // trie par like
+                break
+                case "Date":
+                this.media.sort((a,b) =>  new Date(b.date) - new Date(a.date)); // trie selon la date 
+                break
+                case "Title":
+                this.media.sort((a, b) => a.alt.localeCompare(b.alt, 'fr', {ignorePunctuation: true})); //trie par titre
+            }
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 //IMPORT
 import {recipes} from "./recipes.js"
 //DOM
+const appareilsList = document.getElementById("appareilsList");
+var appliance = []; //tableau appareils
+var ingredients = []; //tableau ingredients
+var ustensils = [] //tableau ustensiles
+var article = undefined
+//FILTER DUPLICATE
+let uniq = unique => [...new Set(unique)];
 //GET JSON FILE
 let myRequest = new Request("./js/data.js") ;
 fetch(myRequest)
@@ -11,12 +18,31 @@ fetch(myRequest)
     .then((data) => {
       const recettes = data.recipes;
       console.log(recettes)
+     
       for (let i in recettes) {
-        var article = new recipes(recettes[i]);
-        
-        article.toHTML();
-        
+        article = new recipes(recettes[i]);
+        article.toHTML(); // recettes
+        article.toHTMLList();
+
+        appliance.push(article.appliance)
+        ingredients.push(article.ingredients)
+        ustensils.push(article.ustensils)
       }
+      
+      
+      
+     
+      appliance = uniq(appliance); // filtre appareils
+      //console.log(appliance)
+      /* ingredients = uniq(ingredients)
+      */
+      appliance.forEach(list => {console.log(list);});
+      //appareilsList.innerHTML += `<li class="appareilsListItem"><button class="button">`+list+`</button></li>`
+      article.ingredients
+
+
+      //add event listener
       article.dropdown();
+      article.search();
     });
     
